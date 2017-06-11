49522a9643c375c482569fe1c1ca6829ecf42b68
philipborg <philip.borgstrom@gmail.com>
2017-06-11 14:07:30 +0200

    Added the forgotten to commit OSN3DTileable

---
+++ src/main/scala/com/philipborg/mummu/math/noise/OSN3DTileable.scala
@@ -0,0 +1,21 @@
+package com.philipborg.mummu.math.noise
+
+import com.kurtspencer.math.noise.OpenSimplexNoiseTileable3D
+import com.google.common.math.IntMath
+
+class OSN3DTileable(seed: Option[Long] = None, width: Long, height: Long, depth: Long) extends Noise3D {
+
+  //Parameter checking
+  if (width % 6 != 0 || height % 6 != 0 || depth % 6 != 0) throw new IllegalArgumentException("OSN3DTileable only supports wrapping dimensions of a multiplier by 6. Example: 6, 12, 18 etc.");
+  if (width / 6 > Int.MaxValue || height / 6 > Int.MaxValue || depth / 6 > Int.MaxValue) throw new IllegalArgumentException("OSN3DTileable only supports wrapping dimensions of at most size 12 884 901 882");
+  
+  protected val osn: OpenSimplexNoiseTileable3D = if (seed.isDefined) {
+    new OpenSimplexNoiseTileable3D(seed.get, (width / 6).toInt, (height / 6).toInt, (depth / 6).toInt);
+  } else {
+    new OpenSimplexNoiseTileable3D((width / 6).toInt, (height / 6).toInt, (depth / 6).toInt);
+  }
+
+  def eval(x: Double, y: Double, z: Double): Double = {
+    osn.eval(x, y, z);
+  }
+}
