093570a503002af88dc5f9aa2958d7252cec3096
philipborg <philip.borgstrom@gmail.com>
2017-06-15 20:18:51 +0200

    Experimental CLI

--- src/main/scala/com/philipborg/mummu/CLI.scala
+++ src/main/scala/com/philipborg/mummu/CLI.scala
@@ -1,34 +1,96 @@
 package com.philipborg.mummu
 
-import java.io.OutputStreamWriter
+import org.apache.commons.cli.HelpFormatter
+import java.awt.GraphicsEnvironment
+import java.nio.charset.Charset
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.CommandLineParser
 import java.nio.charset.StandardCharsets
-
+import org.apache.commons.cli.Options
+import org.apache.commons.cli.CommandLine
+import java.nio.file.Paths
+import java.nio.file.Path
+import java.nio.file.Files
+import java.sql.Time
+import java.util.concurrent.TimeUnit
+import com.philipborg.mummu.io.FileResolver
 import scala.io.StdIn
 
-import com.philipborg.mummu.io.FileResolver
+object CLI {
+  val CLIOptions: Options = new Options {
+    addOption("w", "workspace", true, "The workspace where files are read and written from.");
+    addOption("e", "execute", true, "Execute a ECMA/Java-script relative to the current workspace.");
+    addOption("g", "gui", false, "Open JavaFX based GUI.");
+    addOption("h", "help", false, "Lists all commands.");
+    addOption("hl", "headless", false, "Returns whatever the instance is running in a headless environment.");
+    //addOption("i", "interactive", false, "Activates the interactive ECMA/Java-script mode.");
+    addOption("c", "charset", true, "Charset used to load files.");
+    addOption("t", "timelimit", true, "Time limit for JS Script. 0 is infinite.");
+    addOption("T", "timeunit", true, "Time unit for timelimit.");
+    addOption("e", "exit", false, "Exits the application.");
+  }
+  val parser: CommandLineParser = new DefaultParser();
+}
 
 class CLI {
-  val codeBreaker = "§";
-  def run:Unit = {
-    println("Enter work directory");
-    val dir = StdIn.readLine;
-    val resolver = new FileResolver(dir);
+  def run(args: Array[String]): Unit = {
+    if (args.length > 0) {
+      interpret(args);
+      return ;
+    }
+
     while (true) {
-      println("Write JS then end a line with a " + codeBreaker);
-      var script = "";
-      var completed = false;
-      while (!completed) {
-        val line = StdIn.readLine;
-        if (line.equals(codeBreaker)) {
-          completed = true;
-        } else {
-          script += System.lineSeparator + line;
-        }
-      }
-      val writer = new OutputStreamWriter(System.out, StandardCharsets.UTF_8);
-      val task = new Request(code = script, pathResolver = resolver);
-      val answ = task.call;
-      if (answ.isDefined) throw answ.get;
+      println("Entering interpreter mode.");
+      val line = StdIn.readLine();
+      interpret(line.split(" (?=\")|(?<=\")\\s"));
+    }
+  }
+
+  var workspace: Path = Paths.get(".").toAbsolutePath.normalize;
+  var charset: Charset = StandardCharsets.UTF_8;
+  var timeunit: TimeUnit = TimeUnit.MINUTES;
+  var timelimit: Option[Long] = None;
+
+  protected def interpret(args: Array[String]): Unit = {
+    val cml: CommandLine = CLI.parser.parse(CLI.CLIOptions, args);
+
+    if (cml.hasOption("help")) {
+      val hf = new HelpFormatter;
+      hf.printHelp("mummu", CLI.CLIOptions);
+    }
+    if (cml.hasOption("charset")) {
+      charset = Charset.forName(cml.getOptionValue("charset"));
+      println("Set charset to " + charset.displayName);
+    }
+    if (cml.hasOption("timeunit")) {
+      timeunit = TimeUnit.valueOf(cml.getOptionValue("timeunit"));
+    }
+    if (cml.hasOption("timelimit")) {
+      timelimit = Some(timeunit.toMillis(cml.getOptionValue("timelimit").toLong));
+      if (timelimit.get == 0) timelimit = None;
+    }
+    if (cml.hasOption("headless")) {
+      println(GraphicsEnvironment.isHeadless)
+    }
+    if (cml.hasOption("workspace")) {
+      val suggestedWS = cml.getOptionValue("workspace");
+      val path = Paths.get(suggestedWS);
+      val dirPath = Files.createDirectories(path);
+      workspace = dirPath;
+    }
+    if (cml.hasOption("execute")) {
+      val code = new String(Files.readAllBytes(workspace.resolve(cml.getOptionValue("execute"))), charset);
+      val request = new Request(code = code, pathResolver = new FileResolver(workspace.toAbsolutePath.toString), timelimit);
+      val resp = request.call();
+      if (resp.isDefined) throw resp.get;
+    }
+    if (cml.hasOption("gui")) {
+      val gui = new SimpleGUI;
+      gui.workingDirectory.value = workspace.toAbsolutePath.toString;
+      gui.main(Array.empty[String]);
+    }
+    if (cml.hasOption("exit")) {
+      System.exit(0);
     }
   }
 }
--- src/main/scala/com/philipborg/mummu/Main.scala
+++ src/main/scala/com/philipborg/mummu/Main.scala
@@ -1,9 +1,10 @@
 package com.philipborg.mummu
 
+import java.awt.GraphicsEnvironment
+
 object Main {
   def main(args:Array[String]):Unit = {
-    if(args.length == 0) new SimpleGUI().main(args);
-    else if(args.length > 0) new CLI().run;
-    else throw new IllegalArgumentException;
+    if(args.length == 0 && !GraphicsEnvironment.isHeadless()) new SimpleGUI().main(args);
+    else new CLI().run(args);
   }
 }
